fd64664f0189eaa1681f29f8bdec7642d34d60d7
Rosalind Killingstad <rosalindkillingstad@Rosalinds-MacBook-Pro.local>
2020-04-10 13:44:39 -0600

    updated DB and server

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
-const db = require("../models");
+const db = require("../../models");
 
-router.get("/recipes", (req, res) => {
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
-  db.Recipe.find({
-    title: { $regex: new RegExp(req.query.q, "i") },
-  })
-    .then((recipes) => res.json(recipes))
-    .catch((err) => res.status(422).end());
-});
+// router.get("/user", (req, res) => {
+//   // Use a regular expression to search titles for req.query.q
+//   // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
+//   db.User.find({
+//     title: { $regex: new RegExp(req.query.q, "i") },
+//   })
+//     .then((users) => res.json(users))
+//     .catch((err) => res.status(422).end());
+// });
+
+const userRoutes = require("./users");
+
+// Book routes
+router.use("/users", userRoutes);
+
 
 module.exports = router;
