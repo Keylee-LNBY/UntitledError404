eda9076222f9af4616d2addccc41540406ae4447
Mel Garza <melgarza3016@gmail.com>
2020-04-11 15:07:10 -0500

    Updated signup and login forms for user auth

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -1,44 +1,143 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-const SignUpForm = () => {
-    return (
-        <Form>
-            <Form.Group controlId="formFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="firstName" placeholder="First name" />
-            </Form.Group>
+const useStyles = makeStyles((theme) => ({
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    maxWidth: "768px",
+    margin: "0 auto",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(8),
+      padding: `${theme.spacing(6)}px ${theme.spacing(4)}px`,
+    },
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  buttonProgress: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
+  },
+}));
 
-            <Form.Group controlId="formLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lastName" placeholder="Last name" />
-            </Form.Group>
+const Register = () => {
+  const classes = useStyles({});
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [submitting, setSubmitting] = React.useState(false);
 
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="username" placeholder="Choose your username" />
-            </Form.Group>
+  return (
+    <main className={classes.layout}>
+      <Paper className={classes.paper} elevation={2}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <Typography component="h1" variant="h4" gutterBottom>
+            Register
+          </Typography>
+        </Box>
+        <form method="post" className={classes.form} noValidate>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            autoComplete="fname"
+            autoFocus
+            defaultValue={formData.firstName}
+            onChange={(e) =>
+              setFormData({ ...formData, firstName: e.target.value })
+            }
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="lname"
+            defaultValue={formData.lastName}
+            onChange={(e) =>
+              setFormData({ ...formData, lastName: e.target.value })
+            }
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            defaultValue={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            defaultValue={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+          <Box mb={6}>
+            <Button
+              disabled={submitting}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {submitting && (
+                <CircularProgress
+                  size={24}
+                  className={classes.buttonProgress}
+                />
+              )}
+              {submitting ? "Registering..." : "Register"}
+            </Button>
+          </Box>
+        </form>
+      </Paper>
+    </main>
+  );
+};
 
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-                <Form.Text className="text-muted">
-                    Must be between 8-12 characters and contain letters and numbers
-    </Form.Text>
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Submit
-  </Button>
-        </Form>
-
-    )
-}
-
-export default SignUpForm;
+export default Register;
