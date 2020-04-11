a419371c9093a3888057946ff0171a18ea6a3c30
Rosalind Killingstad <rosalindkillingstad@Rosalinds-MacBook-Pro.local>
2020-04-11 12:01:45 -0600

    updated DB and server

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const Login = () => {
-    return (
-        <h2>Login</h2>
-    )
+const LoginForm = () => {
+  return (
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="username" placeholder="Choose your username" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
-export default Login;
+export default LoginForm;
