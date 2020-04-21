8ddf9f60d53e07d40f1e79568935752042172bf5
Keylee <lineberry.kelly@gmail.com>
2020-04-20 23:46:34 -0400

    containers game

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -2,110 +2,117 @@ import React, { useEffect } from "react"
 import * as PIXI from "pixi.js"
 
 // let snake = () => {
-  let app;
-  let player;
-  let item;
-  let enemy;
-  // let canvas = document.querySelector('body')
-  let score;
-  // let scoreDiv;
-  let keysDiv;
-  let keys = {};
-  // let bg;
+let app;
+let player;
+let item;
+let enemy;
+// let canvas = document.querySelector('body')
+let score;
+// let scoreDiv;
+let keysDiv;
+let keys = {};
+// let bg;
+let startScreen;
+let playScreen;
+let endScreen;
 
-  // const initLevel = () => {
-  //   bg = createBG(app.loader.resources["background"].texture);
 
 
-  //   app.ticker.add(gameLoop);
-  // };
+// const initLevel = () => {
+//   bg = createBG(app.loader.resources["background"].texture);
 
-  const gameLoop = () => {
-      //Directions on Click
-      //Left Arrow
-      if (keys["37"]) {
-          player.x -= 1;
-      }
-      //Up Arrow
-      if (keys["38"]) {
-          player.y -= 1;
-      }
-      //Right Arrow
-      if (keys["39"]) {
-          player.x += 1;
-      }
-      // Down Arrow
-      if (keys["40"]) {
-          player.y += 1;
-      }
 
-      if(rectsIntersect(player, item)) {
-          //Add one point to the scoreDiv
-          score++;
-          //Code that triggers a new item appearing somewhere on the screen
-          drawScore(score);
-          //Move the Item to a new random position
-          setPosition(item);
-      }
+//   app.ticker.add(gameLoop);
+// };
 
-      if(rectsIntersect(player, enemy)) {
+const gameLoop = () => {
+    //Directions on Click
+    //Left Arrow
+    if (keys["37"]) {
+        player.x -= 1;
+    }
+    //Up Arrow
+    if (keys["38"]) {
+        player.y -= 1;
+    }
+    //Right Arrow
+    if (keys["39"]) {
+        player.x += 1;
+    }
+    // Down Arrow
+    if (keys["40"]) {
+        player.y += 1;
+    }
+
+    if(rectsIntersect(player, item)) {
+        //Add one point to the scoreDiv
+        score++;
         //Code that triggers a new item appearing somewhere on the screen
         drawScore(score);
         //Move the Item to a new random position
-        player = 0;
-        enemy = 0;
-
-        // return (`Your Loss. Score: ${score}.`)
+        setPosition(item);
     }
-  };
 
-  //Collision Event 
-  const rectsIntersect = (a,b) => {
-      let aBox = a.getBounds();
-      let bBox = b.getBounds();
+    if(rectsIntersect(player, enemy)) {
+      //Code that triggers a new item appearing somewhere on the screen
+      drawScore(score);
+      //Move the Item to a new random position
+      player = 0;
+      enemy = 0;
 
-      return aBox.x + aBox.width > bBox.x &&
-          aBox.x < bBox.width + bBox.x &&
-          aBox.y + aBox.height > bBox.y &&
-          aBox.y < bBox.y + bBox.height;
+      // return (`Your Loss. Score: ${score}.`)
+  }
+};
 
-  };
+//Collision Event 
+const rectsIntersect = (a,b) => {
+    let aBox = a.getBounds();
+    let bBox = b.getBounds();
 
-  //Push the score to the scoreDiv
-  const drawScore = (a) => {
-      // scoreDiv.innerHTML(`Score: ${a}`);
-      console.log(score);
-  };
+    return aBox.x + aBox.width > bBox.x &&
+        aBox.x < bBox.width + bBox.x &&
+        aBox.y + aBox.height > bBox.y &&
+        aBox.y < bBox.y + bBox.height;
 
-  //Set new position of the item
-  const setPosition = (a) => {
-      a.x = Math.floor(Math.random() * app.view.width);
-      a.y = Math.floor(Math.random() * app.view.height);
-  };
+};
 
-  //Key Functions
-  const keysDown = (i) => {
-      console.log(i.keyCode);
-      keys[i.keyCode] = true;
+//Push the score to the scoreDiv
+const drawScore = (a) => {
+    // scoreDiv.innerHTML(`Score: ${a}`);
+    console.log(score);
+};
 
-          app.ticker.add(gameLoop);
-  };
-  //Don't know if I will need this - could just have it go the direction that you set it go with the down Key
-  const keysUp = (i) => {
-      //console.log(i.keyCode);
-      keys[i.keyCode] = false;
-  };
+//Set new position of the item
+const setPosition = (a) => {
+    a.x = Math.floor(Math.random() * app.view.width);
+    a.y = Math.floor(Math.random() * app.view.height);
+};
 
-    //Creates the background
-    // const createBG = (texture) => {
-    //   let tiling = new PIXI.TilingSprite(texture, 500, 500);
-    //   tiling.position.set(0,0);
-    //   app.state.addChild(tiling);
-  
-    //   return tiling;
-    // };
+//Key Functions
+const keysDown = (i) => {
+    console.log(i.keyCode);
+    keys[i.keyCode] = true;
+
+        app.ticker.add(gameLoop);
+};
+//Don't know if I will need this - could just have it go the direction that you set it go with the down Key
+const keysUp = (i) => {
+    //console.log(i.keyCode);
+    keys[i.keyCode] = false;
+};
+
+  //Creates the background
+  // const createBG = (texture) => {
+  //   let tiling = new PIXI.TilingSprite(texture, 500, 500);
+  //   tiling.position.set(0,0);
+  //   app.state.addChild(tiling);
+
+  //   return tiling;
+  // };
 
 // }
+
+
 
 
 
@@ -122,11 +129,43 @@ const Game = () => {
         );
         //Appends the PIXI Stage element to the stage
         document.querySelector("#gameContainer").appendChild(app.view);   
-  
-        // app.loader.baseUrl = "public";
-        // app.loader.add("background", "background.jpg")
-        // app.loader.onComplete.add(initLevel);
-        // app.loader.load();
+            
+        //Create screen
+        startScreen = new PIXI.Container();
+        playScreen = new PIXI.Container();
+        endScreen = new PIXI.Container();
+
+        playScreen.visible = false;
+        endScreen.visible = false;
+
+        app.state.addChild(startScreen);
+        app.stage.addChild(playScreen);
+        app.state.addChild(endScreen);
+
+        //Set up Title Screen
+        let titleRect = new PIXI.Graphics();
+        titleRect.beginFill(0xFF0000);
+        titleRect.drawRect(0,0,app.view.width, app.view.height);
+        startScreen.addChild(titleRect);
+
+        let startText = new PIXI.Text("Welcome to the OG - Use your arrow keys to collect OK's and Avoid !'s");
+        startText.anchor.set(0.5);
+        startText.x = app.view.width /2;
+        startText.y = app.view.height /2;
+        startText.style = new PIXI.TextStyle({
+          fill: 0x000000,
+          fontSize: 40,
+          fontFamily: "Acme",
+          fontStyle: "bold",
+          strokeThickness: 3
+        });
+        startScreen.addChild(startText)
+
+
+        app.loader.baseUrl = "public";
+        app.loader.add("background", "background.jpg")
+        app.loader.onComplete.add(initLevel);
+        app.loader.load();
   
         //Player Object
         player = new PIXI.Sprite.from("./player.png");
