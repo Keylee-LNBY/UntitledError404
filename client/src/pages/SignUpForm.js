6c14e7b945348c900b3ddcc7658a8d6bf27c9d75
Syd Rigel <prest261@umn.edu>
2020-04-21 14:32:46 -0600

    nothing works, don't merge

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -1,60 +1,24 @@
 import React, { useState } from "react";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import Box from "@material-ui/core/Box";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import TextField from "@material-ui/core/TextField";
+// import Button from "@material-ui/core/Button";
 // import CircularProgress from "@material-ui/core/CircularProgress";
 import API from "../utils/API";
 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const useStyles = makeStyles((theme) => ({
-  layout: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    maxWidth: "768px",
-    margin: "0 auto",
-  },
-  paper: {
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(8),
-      padding: `${theme.spacing(6)}px ${theme.spacing(4)}px`,
-    },
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  buttonProgress: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    marginTop: -12,
-    marginLeft: -12,
-  },
-}));
 
 const Register = () => {
-  const classes = useStyles({});
   const [userInput, setUserInput] = useState({});
 
   const handleInputChange = e => {
     const { name, value } = e.target;
     setUserInput({ ...userInput, [name]: value });
   };
-  // const [formData, setFormData] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   username: "",
-  //   password: "",
-  // });
-
 
   const handleFormSubmit = e => {
     e.preventDefault();
@@ -71,106 +35,123 @@ const Register = () => {
         });
     }
   };
-  // const [submitting, setSubmitting] = useState(false);
 
   console.log("user input", userInput)
 
   return (
-    <div classname="container">
-      <main className={classes.layout}>
-        <Paper className={classes.paper} elevation={2}>
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            flexDirection="column"
-          >
-            <Typography component="h1" variant="h4" gutterBottom>
-              Register
-          </Typography>
-          </Box>
-          <form method="post" className={classes.form} noValidate>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              name="firstName"
-              autoComplete="fname"
-              autoFocus
-              defaultValue={userInput.firstName}
-              onChange={handleInputChange}
-            // {(e) =>
-            //   setUserInput({ ...userInput, firstName: e.target.value })
-            // }
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="lname"
-              defaultValue={userInput.lastName}
-              onChange={handleInputChange}
-            // {(e) =>
-            //   setUserInput({ ...userInput, lastName: e.target.value })
-            // }
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Email"
-              name="username"
-              autoComplete="username"
-              defaultValue={userInput.username}
-              onChange={handleInputChange}
-            // {(e) =>
-            //   setUserInput({ ...userInput, username: e.target.value })
-            // }
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-              defaultValue={userInput.password}
-              onChange={handleInputChange}
-            // {(e) =>
-            //   setUserInput({ ...userInput, password: e.target.value })
-            // }
-            />
-            <Box mb={6}>
-              <Button onClick={handleFormSubmit}
-                // disabled={handleFormSubmit}
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-              >
-                {/* {handleFormSubmit && (
-                <CircularProgress
-                  size={24}
-                  className={classes.buttonProgress}
-                />
-              )} */}
-              Register
-            </Button>
-            </Box>
-          </form>
-        </Paper>
-      </main>
+    <div>
+      <Form>
+        <Form.Group controlId="formFirstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="firstName" placeholder="First Name" defaultValue={userInput.firstName} onChange={handleInputChange}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lastName" placeholder="Last Name" defaultValue={userInput.lastName} onChange={handleInputChange}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="username" placeholder="Username" defaultValue={userInput.username} onChange={handleInputChange}></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" defaultValue={userInput.password} onChange={handleInputChange}></Form.Control>
+          <Form.Text classname="text-muted">Must be between 8-12 characters</Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit" onChange={handleFormSubmit}>Register</Button>
+      </Form>
     </div>
+
+    // <div classname="container">
+    //   <main className={classes.layout}>
+    //     <Paper className={classes.paper} elevation={2}>
+    //       <Box
+    //         display="flex"
+    //         alignItems="center"
+    //         justifyContent="center"
+    //         flexDirection="column"
+    //       >
+    //         <Typography component="h1" variant="h4" gutterBottom>
+    //           Register
+    //       </Typography>
+    //       </Box>
+    //       <form method="post" className={classes.form} noValidate>
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           id="firstName"
+    //           label="First Name"
+    //           name="firstName"
+    //           autoComplete="fname"
+    //           autoFocus
+    //           defaultValue={userInput.firstName}
+    //           onChange={handleInputChange}
+    //         // {(e) =>
+    //         //   setUserInput({ ...userInput, firstName: e.target.value })
+    //         // }
+    //         />
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           id="lastName"
+    //           label="Last Name"
+    //           name="lastName"
+    //           autoComplete="lname"
+    //           defaultValue={userInput.lastName}
+    //           onChange={handleInputChange}
+    //         // {(e) =>
+    //         //   setUserInput({ ...userInput, lastName: e.target.value })
+    //         // }
+    //         />
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           id="username"
+    //           label="Email"
+    //           name="username"
+    //           autoComplete="username"
+    //           defaultValue={userInput.username}
+    //           onChange={handleInputChange}
+    //         // {(e) =>
+    //         //   setUserInput({ ...userInput, username: e.target.value })
+    //         // }
+    //         />
+    //         <TextField
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           name="password"
+    //           label="Password"
+    //           type="password"
+    //           id="password"
+    //           autoComplete="new-password"
+    //           defaultValue={userInput.password}
+    //           onChange={handleInputChange}
+    //         // {(e) =>
+    //         //   setUserInput({ ...userInput, password: e.target.value })
+    //         // }
+    //         />
+    //         <Box mb={6}>
+    //           <Button onClick={handleFormSubmit}
+    //             // disabled={handleFormSubmit}
+    //             type="submit"
+    //             fullWidth
+    //             variant="contained"
+    //             color="primary"
+    //             className={classes.submit}
+    //           >
+    //             Register
+    //         </Button>
+    //         </Box>
+    //       </form>
+    //     </Paper>
+    //   </main>
+    // </div >
   );
 };
+
 
 export default Register;
