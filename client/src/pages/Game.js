77be1dfbd1efcee6918ee95653513bb49bc90171
Keylee <lineberry.kelly@gmail.com>
2020-04-20 21:43:22 -0400

    the display is now within the container

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -6,29 +6,36 @@ let snake = () => {
   let app;
   let player;
   let item;
+  let enemy;
   // let canvas = document.querySelector('body')
   let score;
   // let scoreDiv;
   let keysDiv;
   let keys = {};
+  // let bg;
 
   //When the page loads create teh pixi stage and append it to the page
   window.onload = () => {
       app = new PIXI.Application(
           {
-              width: window.innerWidth,
-              height: window.innerHeight,
+              width: 500,
+              height: 500,
               background: 0x000000,
           }
       );
       //Appends the PIXI Stage element to the stage
-      document.body.appendChild(app.view);   
+      document.querySelector(".container").appendChild(app.view);   
+
+      // app.loader.baseUrl = "public";
+      // app.loader.add("background", "background.jpg")
+      // app.loader.onComplete.add(initLevel);
+      // app.loader.load();
 
       //Player Object
       player = new PIXI.Sprite.from("./player.png");
       player.anchor.set(0.5);
       player.x = app.view.width /2;
-      player.y = app.view.width /2;
+      player.y = app.view.height /2;
 
       app.stage.addChild(player);
 
@@ -36,9 +43,17 @@ let snake = () => {
       item = new PIXI.Sprite.from("./item.png");
       item.anchor.set(0);
       item.x = Math.floor(Math.random() * app.view.width);
-      item.y = Math.floor(Math.random() * app.view.width);
+      item.y = Math.floor(Math.random() * app.view.height);
 
       app.stage.addChild(item);
+
+      //Enemy Object
+      enemy = new PIXI.Sprite.from("./obstError.png");
+      enemy.anchor.set(0);
+      enemy.x = Math.floor(Math.random() * app.view.width);
+      enemy.y = Math.floor(Math.random() * app.view.height);
+
+      app.stage.addChild(enemy);
 
       //Keyboard Event Handlers
       window.addEventListener("keydown", keysDown);
@@ -49,7 +64,14 @@ let snake = () => {
       keysDiv = document.querySelector('#keys');
       // scoreDiv = document.querySelector('#scoreBoard');
       score = 0;
-  }
+  };
+
+  // const initLevel = () => {
+  //   bg = createBG(app.loader.resources["background"].texture);
+
+
+  //   app.ticker.add(gameLoop);
+  // };
 
   const gameLoop = () => {
       //Directions on Click
@@ -78,7 +100,17 @@ let snake = () => {
           //Move the Item to a new random position
           setPosition(item);
       }
-  }
+
+      if(rectsIntersect(player, enemy)) {
+        //Code that triggers a new item appearing somewhere on the screen
+        drawScore(score);
+        //Move the Item to a new random position
+        player = 0;
+        enemy = 0;
+
+        // return (`Your Loss. Score: ${score}.`)
+    }
+  };
 
   //Collision Event 
   const rectsIntersect = (a,b) => {
@@ -90,19 +122,19 @@ let snake = () => {
           aBox.y + aBox.height > bBox.y &&
           aBox.y < bBox.y + bBox.height;
 
-  }
+  };
 
   //Push the score to the scoreDiv
   const drawScore = (a) => {
       // scoreDiv.innerHTML(`Score: ${a}`);
       console.log(score);
-  }
+  };
 
   //Set new position of the item
   const setPosition = (a) => {
       a.x = Math.floor(Math.random() * app.view.width);
-      a.y = Math.floor(Math.random() * app.view.width);
-  }
+      a.y = Math.floor(Math.random() * app.view.height);
+  };
 
   //Key Functions
   const keysDown = (i) => {
@@ -116,6 +148,15 @@ let snake = () => {
       //console.log(i.keyCode);
       keys[i.keyCode] = false;
   };
+
+    //Creates the background
+    // const createBG = (texture) => {
+    //   let tiling = new PIXI.TilingSprite(texture, 500, 500);
+    //   tiling.position.set(0,0);
+    //   app.state.addChild(tiling);
+  
+    //   return tiling;
+    // };
 
 }
 
